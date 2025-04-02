a11c77dbd93d68b9e9b8698943421ed6f9841d88
knock-huynh <huynhtransongngoc@gmail.com>
2025-04-02 21:57:40 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
